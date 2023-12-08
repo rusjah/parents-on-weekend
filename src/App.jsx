9a86aafdcda3d32c463849d8f33a47a06981aebe
ruslana_hural <rusjah1@gmail.com>
2023-12-08 13:36:59 +0100

    adding footer :

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Footer from './components/layout/Footer';
 
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
           <Route path='/' element={<Home />} />
 
       </Routes>
-
+      <Footer />
     </div>
   );
 }
