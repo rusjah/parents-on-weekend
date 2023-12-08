7d479e3ceeedaf0f06b1cb2cf3d805bd9547a5bd
ruslana_hural <rusjah1@gmail.com>
2023-12-08 14:40:28 +0100

    adding nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Footer from './components/layout/Footer';
+import Nav from './components/layout/Nav';
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Routes>
-          <Route path='/' element={<Home />} />
+      <div className='min-h-[66vh]'>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            
 
-      </Routes>
+        </Routes>
+      </div>
+      
       <Footer />
     </div>
   );
