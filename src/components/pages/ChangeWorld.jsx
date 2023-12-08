691f2ba3adafdb5bad3d2f34193d6d42e529973d
ruslana_hural <rusjah1@gmail.com>
2023-12-08 13:14:33 +0100

    adding components of main pages

---
+++ src/components/pages/ChangeWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChangeWorld() {
+  return (
+    <div>ChangeWorld</div>
+  )
+}
+
+export default ChangeWorld
---
+++ src/components/pages/listOfOffers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function listOfOffers() {
+  return (
+    <div>listOfOffers</div>
+  )
+}
+
+export default listOfOffers
