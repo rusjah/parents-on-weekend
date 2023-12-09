ba48a2ceeb1c5a7d92cb8a57637bb7e4b499f088
ruslana_hural <rusjah1@gmail.com>
2023-12-09 03:59:34 +0100

    adding static review

---
+++ src/components/components/FullReviweCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import RatingStars from './RatingStars'
+
+function FullReviweCard({user}) {
+  return (
+    <div className="card w-full bg-orange-50 shadow-xl w-96 h-64">
+        <div className="card-body">
+            <div className='flex gap-4'>
+                <figure className='w-12 h-12'>
+                    <img className='w-full h-full rounded-full' src={user.photo} alt="photo" />
+                </figure>
+                <figcaption>
+                    <p className='font-bold text-yellow-995'>{user.fname} {user.lname}</p>
+                    <div className="rating">
+                        <p className='mask mask-star-2 bg-orange-400 w-6 h-6'></p>
+                        <RatingStars rating={user.rating} />
+                    </div>
+                </figcaption>
+            </div>
+            <div className="">"{user.reviw}" I had a fantastic experience using "Grandparents on weekend". The interface is user-friendly, offering a wide range of services/products. Excellent customer service and a smooth transaction process. Highly recommended!'I had a fantastic experience using "Grandparents on weekend". The interface is user-friendly, offering a wide range of services/products. Excellent customer service and a smooth transaction process. Highly recommended!'</div>
+        </div>
+    </div>
+  )
+}
+
+export default FullReviweCard
--- src/components/pages/Reviews.jsx
+++ src/components/pages/Reviews.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import AddReviw from '../components/AddReviw'
+import FullReviweCard from '../components/FullReviweCard'
 
 function Reviews() {
+  const user =  {
+    fname: 'Edward',
+    lname: "Ksija",
+    photo: "https://pivotalmist.backendless.app/api/files/photos/users/father.jpg",
+    reviw: 'I had a fantastic experience using "Grandparents on weekend". The interface is user-friendly, offering a wide range of services/products. Excellent customer service and a smooth transaction process. Highly recommended!',
+    rating: 5
+}
+
   return (
     <div className='flex flex-col justify-center items-center '>
        <h2 className='font-bold text-[2rem] text-green-900 pb-4'>All Reviws</h2>
       <AddReviw />
-      
+      <div className='flex flex-col gap-8 p-12'>
+        <FullReviweCard user={user} />
+        <FullReviweCard user={user} />
+        <FullReviweCard user={user} />
+      </div>
     </div>
   )
 }
